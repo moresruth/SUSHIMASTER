4b50ab8ddff74e83b8bb4ef88a738f564fa4e6aa
Ruth Mores <moresruthantonette@gmail.com>
2023-10-07 12:08:55 +0800

    add responsive design on testimonial

--- src/Container/Testimonial/Testimonial.jsx
+++ src/Container/Testimonial/Testimonial.jsx
@@ -7,9 +7,13 @@ const Testimonial = () => {
   return (
     <section className="app__testimonial app__section">
       <div className="app__testimonial-content app__wrapper">
-        <div className="app__testimonial-content_leftside">
+        <div className="app__testimonial-content_leftside ">
           <h1 className="app__flex">What Our Customers Are Saying</h1>
-          <img src={images.testimonial} alt="testimonial img" />
+          <img
+            src={images.testimonial}
+            alt="testimonial img"
+            className="app__flex"
+          />
         </div>
         <div className="app__testimonial-item">
           <TestimonialCard />
