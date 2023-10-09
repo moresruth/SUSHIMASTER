3821338f598fbe3da25d22ba8c2e94e9b9cdd38d
Ruth Mores <moresruthantonette@gmail.com>
2023-10-10 00:16:09 +0800

    update

--- src/Container/Testimonial/Testimonial.jsx
+++ src/Container/Testimonial/Testimonial.jsx
@@ -5,20 +5,22 @@ import { TestimonialCard } from "../../Components";
 
 const Testimonial = () => {
   return (
-    <section className="app__testimonial app__section" id="testimonial">
-      <div className="app__testimonial-content app__wrapper">
-        <div className="app__testimonial-content_leftside ">
-          <h1 className="app__flex">What Our Customers Are Saying</h1>
-          <img
-            src={images.testimonial}
-            alt="testimonial img"
-            className="app__flex"
-          />
-        </div>
-        <div className="app__testimonial-item">
-          <TestimonialCard />
-          <TestimonialCard />
-          <TestimonialCard />
+    <section className="app__testimonial" id="testimonial">
+      <div className="app__wrapper">
+        <div className="app__testimonial-content ">
+          <div className="app__testimonial-content_leftside ">
+            <h1 className="app__flex">What Our Customers Are Saying</h1>
+            <img
+              src={images.testimonial}
+              alt="testimonial img"
+              className="app__flex"
+            />
+          </div>
+          <div className="app__testimonial-item">
+            <TestimonialCard />
+            <TestimonialCard />
+            <TestimonialCard />
+          </div>
         </div>
       </div>
     </section>
